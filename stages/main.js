93541e0738ede8517b8ab3335114e50ed6a28771
karim_i35_ewd15 <karim.abdelmohsen.1992@gmail.com>
2015-03-06 19:25:08 +0200

    adding space and wasps stages

--- stages/main.js
+++ stages/main.js
@@ -15,17 +15,18 @@ game.state.add('scoreStage', scoreStage);          // the score stage
 game.state.add('game', gameStage);          // the game stage
 
 game.state.add('electricityStage', electricityStage);          // the cable stage
-game.state.add('electricity_success', electricitySuccess);          // the game stage
+//game.state.add('electricity_success', electricitySuccess);          // the game stage
 game.state.add('electricity_fail', electricityFail);          // the game stage
-game.state.add('cableStage', cableStage);          // the cable stage
+//game.state.add('cableStage', cableStage);          // the cable stage
 game.state.add('forkStage', forkStage);            // the fork stage
 game.state.add('runningStage', runningStage);            // the fork stage
 game.state.add('pokeTheBearStage', pokeTheBearStage); //the poke stage
-
+game.state.add('spaceStage', spaceStage); //the space stage
+game.state.add('waspsStage', waspsStage); //the space stage
 // globals
 var globals = {
 	// add your stage to this stages array to make the game choose randomly from it
-    stages: ['cableStage', 'forkStage', 'runningStage', 'pokeTheBearStage'],
+    stages: ['cableStage', 'forkStage', 'runningStage', 'pokeTheBearStage','spaceStage','waspsStage'],
 
     score: 0,
     lives: 3
