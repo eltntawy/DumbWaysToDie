fc866ab5bf14b280626cef7d9793caaefbd3d42b
Hossam.AHamid <hossam.ahamid@gmail.com>
2015-03-08 00:51:24 +0200

    pokeTheBearStage added  animations

--- stages/poke.js
+++ stages/poke.js
@@ -1,10 +1,16 @@
 var pokeTheBearStage = {
     preload: function(){
-        game.load.image('dummy', 'assets/pokeTheBear/dummy.png');
-        game.load.image('bearBelly', 'assets/pokeTheBear/bearBelly.png');
+
         game.load.image('pokeBar', 'assets/pokeTheBear/pokeBar.png');
         game.load.image('pokeSafe', 'assets/pokeTheBear/pokeSafe.png');
         game.load.image('bearFace', 'assets/pokeTheBear/bearFace.png');
+        game.load.image('stick', 'assets/pokeTheBear/stick.png');
+
+
+        game.load.atlasJSONHash('dummySprite', 'assets/pokeTheBear/dummy.png', 'assets/pokeTheBear/dummy.json');
+        game.load.atlasJSONHash('bearBellySprite', 'assets/pokeTheBear/bearBelly.png', 'assets/pokeTheBear/bearBelly.json');
+        game.load.atlasJSONHash('bearEatingSprite', 'assets/pokeTheBear/bearOpenEating.png', 'assets/pokeTheBear/bearOpenEating.json');
+
     },
 
     create: function(){
@@ -12,12 +18,23 @@ var pokeTheBearStage = {
         game.stage.backgroundColor = "#88bac3";
 
         this.initialStartX = game.world.centerX;
+        this.trials = 0;
+        this.win = false;
 
-        this.dummy = game.add.sprite(this.game.world.centerX + 140, game.world.centerY + 20, 'dummy');
+        this.dummy = game.add.sprite(this.game.world.centerX + 170, game.world.centerY + 10, 'dummySprite');
         this.dummy.anchor.set(.5,.5);
+        this.dummy.animations.add('pokeAnime', [0, 1, 2, 3], 10);
+        this.dummy.animations.add('dummyEaten', [4, 5, 6, 7], 10);
+        this.dummy.animations.add('dummyLower', [8, 9, 10, 11, 12], 10);
 
-        this.bearBelly = game.add.sprite(this.game.world.width, 0, 'bearBelly');
+
+        this.stick = game.add.sprite(game.world.centerX + 240, game.world.centerY - 32, 'stick');
+        this.stick.anchor.set(0,0);
+        this.stick.alpha = 0;
+
+        this.bearBelly = game.add.sprite(this.game.world.width, 0, 'bearBellySprite');
         this.bearBelly.anchor.set(1,0);
+        this.bearBelly.animations.add('bearBellyPoked', [], 5);
 
         this.pokeBar = game.add.sprite(this.initialStartX, game.world.height - 100, 'pokeBar');
         this.pokeBar.anchor.set(.5,.5);
@@ -28,8 +45,13 @@ var pokeTheBearStage = {
         this.bearFace = game.add.sprite(this.initialStartX, game.world.height - 100, 'bearFace');
         this.bearFace.anchor.set(.5,.5);
 
+        this.bearEating = game.add.sprite(game.world.centerX + 170, 0, 'bearEatingSprite');
+        this.bearEating.anchor.set(0.5, 1);
+        this.bearEating.animations.add('bearOpeningMouth', [], 9);
+
         game.physics.arcade.enable(this.bearFace);
         game.physics.arcade.enable(this.pokeSafe);
+        game.physics.arcade.enable(this.bearEating);
 
         this.bearFace.body.velocity.x = 300;
 
@@ -42,10 +64,10 @@ var pokeTheBearStage = {
         this.preloadBar.scale.y = 3;
 
         // set the time after which the game ends
-        game.time.events.add(Phaser.Timer.SECOND * globals.duration, this.endStage, this);
+        game.time.events.add(Phaser.Timer.SECOND * this.duration, this.endStage, this);
 
         // moves duration bar
-        game.time.events.repeat(Phaser.Timer.SECOND / 20, globals.duration * 20, this.decreaseTimer, this);
+        game.time.events.repeat(Phaser.Timer.SECOND / 20, this.duration * 20, this.decreaseTimer, this);
 
         console.log(this.initialStartX - (this.pokeSafe.width / 2 - 32));
         console.log(this.initialStartX + (this.pokeSafe.width / 2 - 32));
@@ -54,29 +76,74 @@ var pokeTheBearStage = {
     },
 
     update: function(){
+
+        if(this.bearEating.position.y < 0){
+            this.bearEating.body.velocity.y = 0;
+            this.bearEating.body.velocity.x = 0;
+            this.bearEating.position.x = game.world.centerX + 170;
+            this.bearEating.position.y = 0;
+        }
+
         if (this.bearFace.position.x >= this.initialStartX + (this.pokeBar.width / 2 - 40) && this.bearFace.body.velocity.x > 0)
             this.bearFace.body.velocity.x = this.bearFace.body.velocity.x * -1;
         else if(this.bearFace.position.x <= this.initialStartX - (this.pokeBar.width / 2 - 40) && this.bearFace.body.velocity.x < 0)
             this.bearFace.body.velocity.x = this.bearFace.body.velocity.x * -1;
 
-        if (game.input.activePointer.isDown){
+        game.input.onDown.addOnce(this.poked, this);
 
-            //if (game.physics.arcade.overlap(this.bearFace, this.pokeSafe))
-            //    console.log("got it");
-
-            if(this.pokeSafe.getBounds().containsRect(this.bearFace.getBounds()))
-                console.log("got it");
-        }
     },
 
+    poked: function(){
+
+
+        this.stick.alpha = 1;
+        this.bearBelly.animations.play('bearBellyPoked');
+
+
+        this.bearEating.body.velocity.x = -200;
+        this.bearEating.body.velocity.y = 700;
+        this.bearEating.animations.play('bearOpeningMouth');
+        this.bearEating.events.onAnimationComplete.add(function(){
+            this.stick.alpha = 0;
+
+            this.bearEating.body.velocity.y = -800;
+            this.bearEating.body.velocity.x = 200;
+
+        }, this);
+
+        //dummy escaped the eating bear
+        if(this.pokeSafe.getBounds().containsRect(this.bearFace.getBounds())){
+            console.log("got it");
+
+            this.dummy.animations.stop(null, true);
+            this.dummy.animations.play('dummyLower');
+
+            this.dummy.events.onAnimationComplete.add(function(){
+                this.trials++;
+                console.log(this.trials);
+                if(this.trials == 2){
+                    this.win = true;
+                    this.endStage();
+                }
+            }, this);
+        }
+        else{
+            this.dummy.animations.play('dummyEaten');
+            this.endStage();
+        }
+
+    },
     // time allocated for stage
     duration: 5,
 
     endStage: function() {
-        game.state.start('scoreStage');
+        if (this.win == true)
+            game.state.start('scoreStage');
+        else
+            game.state.start('scoreStage');
     },
 
     decreaseTimer: function () {
-        this.preloadBar.scale.x -= 1/globals.duration/20;
+        this.preloadBar.scale.x -= 1/this.duration/20;
     }
-}
+};
