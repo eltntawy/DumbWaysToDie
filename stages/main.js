ce31653e013ed17b6110a031bc4c1e88f9a45537
sharno <sharnoby3@gmail.com>
2015-03-08 09:13:58 +0200

    fixed some merge conflicts

--- stages/main.js
+++ stages/main.js
@@ -30,7 +30,7 @@ game.state.add('boot', boot); // boot  stage
 // globals
 var globals = {
 	// add your stage to this stages array to make the game choose randomly from it
-    stages: ['electricityStage', 'forkStage', 'runningStage', 'pokeTheBearStage','spaceStage', 'trainStage', 'waspsStage','dontPressTheButtonStage'],
+    stages: ['electricityStage', 'forkStage', 'runningStage', 'spaceStage', 'trainStage', 'waspsStage','dontPressTheButtonStage', 'pokeTheBearStage'],
     //stages: ['dontPressTheButtonStage'],
     score: 0,
     lives: 3 ,
